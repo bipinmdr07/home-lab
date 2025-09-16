145895aa72cbfa9227f9fd757a29b4a27da170c6
Bipin Manandhar <kubipin07@gmail.com>
2025-09-16 22:35:44 +0545

    Add nextcloud to service list.

--- services/postgres/custom-scripts/create-databases.sql
+++ services/postgres/custom-scripts/create-databases.sql
@@ -1,2 +1,3 @@
 -- Create databases if they do not exist
 SELECT 'CREATE DATABASE passbolt;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'passbolt')\gexec
+SELECT 'CREATE DATABASE nextcloud;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'nextcloud')\gexec
