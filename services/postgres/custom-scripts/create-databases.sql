10ac5de7bd0d058b4f7ff9052811ed9d70519b88
Bipin Manandhar <kubipin07@gmail.com>
2025-09-17 20:24:24 +0545

    Add immich to service list.

--- services/postgres/custom-scripts/create-databases.sql
+++ services/postgres/custom-scripts/create-databases.sql
@@ -1,3 +1,4 @@
 -- Create databases if they do not exist
 SELECT 'CREATE DATABASE passbolt;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'passbolt')\gexec
 SELECT 'CREATE DATABASE nextcloud;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'nextcloud')\gexec
+SELECT 'CREATE DATABASE immich;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'immich')\gexec
