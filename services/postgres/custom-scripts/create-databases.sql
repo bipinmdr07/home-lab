04119bca9d0cc6a112cd37bedda9997946d6fa63
Bipin Manandhar <kubipin07@gmail.com>
2025-10-11 09:29:16 +0000

    Fix the docker file for n8n and configure n8n to use postgres database

--- services/postgres/custom-scripts/create-databases.sql
+++ services/postgres/custom-scripts/create-databases.sql
@@ -2,3 +2,4 @@
 SELECT 'CREATE DATABASE passbolt;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'passbolt')\gexec
 SELECT 'CREATE DATABASE nextcloud;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'nextcloud')\gexec
 SELECT 'CREATE DATABASE immich;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'immich')\gexec
+SELECT 'CREATE DATABASE n8n;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'n8n')\gexec
