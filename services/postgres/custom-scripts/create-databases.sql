f3f8074c07240c0c57238f990f309d6cfe575d37
Bipin Manandhar <kubipin07@gmail.com>
2026-03-21 17:19:19 +0000

    Replace booklore with grimmory and add postiz service

--- services/postgres/custom-scripts/create-databases.sql
+++ services/postgres/custom-scripts/create-databases.sql
@@ -3,3 +3,6 @@ SELECT 'CREATE DATABASE passbolt;' WHERE NOT EXISTS (SELECT FROM pg_database WHE
 SELECT 'CREATE DATABASE nextcloud;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'nextcloud')\gexec
 SELECT 'CREATE DATABASE immich;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'immich')\gexec
 SELECT 'CREATE DATABASE n8n;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'n8n')\gexec
+SELECT 'CREATE DATABASE postiz;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'postiz')\gexec
+SELECT 'CREATE DATABASE temporal;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'temporal')\gexec
+SELECT 'CREATE DATABASE temporal_visibility;' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'temporal_visibility')\gexec
